d4febf10fb14d7cfc47f2ff63d534f43c30ca960
Angelina <bunny.iblis@gmail.com>
2023-01-24 14:49:37 +0300

    AR button

--- js/main.js
+++ js/main.js
@@ -1,17 +1,8 @@
 import * as THREE from '../../libs/three.js-r132/build/three.module.js';
+import {ARButton} from '../../libs/three.js-r132/examples/jsm/webxr/ARButton.js';
 
 document.addEventListener('DOMContentLoaded', () => {
   const initialize = async() => {
-    const arButton = document.querySelector("#ar-button");
-
-    // check and request webxr session 
-    const supported = navigator.xr && await navigator.xr.isSessionSupported('immersive-ar');
-    if (!supported) {
-      arButton.textContent = "Not Supported";
-      arButton.disabled = true;
-      return;
-    }
-
     // build three.js scene
     const scene = new THREE.Scene();
     const camera = new THREE.PerspectiveCamera();
@@ -31,41 +22,13 @@ document.addEventListener('DOMContentLoaded', () => {
     const light = new THREE.HemisphereLight( 0xffffff, 0xbbbbff, 1 );
     scene.add(light);
 
-
-    renderer.xr.addEventListener("sessionstart", (e) => {
-      console.log("session start");
-    });
-    renderer.xr.addEventListener("sessionend", () => {
-      console.log("session end");
+    renderer.xr.enabled = true;
+    renderer.setAnimationLoop(() => {
+	    renderer.render(scene, camera);
     });
 
-    let currentSession = null;
-    const start = async() => {
-      currentSession = await navigator.xr.requestSession('immersive-ar', {optionalFeatures: ['dom-overlay'], domOverlay: {root: document.body}});
-
-      renderer.xr.enabled = true;
-      renderer.xr.setReferenceSpaceType('local');
-      await renderer.xr.setSession(currentSession);
-      arButton.textContent = "End";
-
-      renderer.setAnimationLoop(() => {
-	renderer.render(scene, camera);
-      });
-    }
-    const end = async() => {
-      currentSession.end();
-      renderer.setAnimationLoop(null);
-      renderer.clear();
-      arButton.style.display = "none";
-    }
-    arButton.addEventListener('click', () => {
-      if (currentSession) {
-	end();
-      } else {
-	start();
-      }
-    });
-
+    const arButton = ARButton.createButton(renderer, {optionalFeatures: ['dom-overlay'], domOverlay: {root: document.body}});
+    document.body.appendChild(arButton);
   }
 
   initialize();

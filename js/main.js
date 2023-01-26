b5497435f1414d889896f84a1ca35ad07c7aea8e
Angelina <bunny.iblis@gmail.com>
2023-01-26 15:51:57 +0300

    Place object on the floor

--- js/main.js
+++ js/main.js
@@ -3,32 +3,68 @@ import {ARButton} from '../../libs/three.js-r132/examples/jsm/webxr/ARButton.js'
 
 document.addEventListener('DOMContentLoaded', () => {
   const initialize = async() => {
-    // build three.js scene
     const scene = new THREE.Scene();
     const camera = new THREE.PerspectiveCamera();
-
-    const renderer = new THREE.WebGLRenderer({antialias: true, alpha: true});
-    renderer.setSize(window.innerWidth, window.innerHeight);
-    renderer.setPixelRatio(window.devicePixelRatio);
-    document.body.appendChild(renderer.domElement);
-
-    // create AR object
-    const geometry = new THREE.BoxGeometry(0.06, 0.06, 0.06); 
-    const material = new THREE.MeshBasicMaterial({color: 0x00ff00});
-    const mesh = new THREE.Mesh(geometry, material);
-    mesh.position.set(0, 0, -0.3);
-    scene.add(mesh);
 
     const light = new THREE.HemisphereLight( 0xffffff, 0xbbbbff, 1 );
     scene.add(light);
 
+    const reticleGeometry = new THREE.RingGeometry( 0.15, 0.2, 32 ).rotateX(- Math.PI / 2);
+    const reticleMaterial = new THREE.MeshBasicMaterial(); 
+    const reticle = new THREE.Mesh(reticleGeometry, reticleMaterial);
+    reticle.matrixAutoUpdate = false;
+    reticle.visible = false;
+    scene.add(reticle);
+
+    const renderer = new THREE.WebGLRenderer({antialias: true, alpha: true});
+    renderer.setPixelRatio(window.devicePixelRatio);
+    renderer.setSize(window.innerWidth, window.innerHeight);
     renderer.xr.enabled = true;
-    renderer.setAnimationLoop(() => {
-	    renderer.render(scene, camera);
+
+    const arButton = ARButton.createButton(renderer, {requiredFeatures: ['hit-test'], optionalFeatures: ['dom-overlay'], domOverlay: {root: document.body}});
+    document.body.appendChild(renderer.domElement);
+    document.body.appendChild(arButton);
+
+    const controller = renderer.xr.getController(0);
+    scene.add(controller);
+    controller.addEventListener('select', () => {
+      const geometry = new THREE.BoxGeometry(0.06, 0.06, 0.06); 
+      const material = new THREE.MeshBasicMaterial({ color: 0xffffff * Math.random()});
+      const mesh = new THREE.Mesh(geometry, material);
+      mesh.position.setFromMatrixPosition(reticle.matrix);
+      mesh.scale.y = Math.random() * 2 + 1;
+      scene.add(mesh);
     });
 
-    const arButton = ARButton.createButton(renderer, {optionalFeatures: ['dom-overlay'], domOverlay: {root: document.body}});
-    document.body.appendChild(arButton);
+    renderer.xr.addEventListener("sessionstart", async (e) => {
+      const session = renderer.xr.getSession();
+      const viewerReferenceSpace = await session.requestReferenceSpace("viewer");
+      const hitTestSource = await session.requestHitTestSource({space: viewerReferenceSpace});
+
+      renderer.setAnimationLoop((timestamp, frame) => {
+	if (!frame) return;
+
+	const hitTestResults = frame.getHitTestResults(hitTestSource);
+
+	if (hitTestResults.length) {
+	  const hit = hitTestResults[0];
+	  const referenceSpace = renderer.xr.getReferenceSpace(); // ARButton requested 'local' reference space
+	  const hitPose = hit.getPose(referenceSpace);
+
+	  reticle.visible = true;
+	  reticle.matrix.fromArray(hitPose.transform.matrix);
+	} else {
+	  reticle.visible = false;
+	}
+
+	renderer.render(scene, camera);
+      });
+    });
+
+    renderer.xr.addEventListener("sessionend", () => {
+      console.log("session end");
+    });
+
   }
 
   initialize();
